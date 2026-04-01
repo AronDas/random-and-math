fb823ff17beee9c42deb33ba4a2d87bb1771f0e9
AronDas <arondas.sg@gmail.com>
2026-04-01 19:07:13 -0300

    Add files via upload

---
+++ trigonomic value.py
@@ -0,0 +1,5 @@
+import math
+number = int(input("Please enter the degree you want to calculate the trigonomic value:"))
+print("sin value of", number, "is", math.sin(number))
+print("sin value of", number, "is", math.cos(number))
+print("sin value of", number, "is", math.tan(number))
